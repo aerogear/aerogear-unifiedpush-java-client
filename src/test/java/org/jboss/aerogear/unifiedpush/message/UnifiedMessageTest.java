8360313b8216bc5effe0bece2f4efc7ffc0651a5
Matthias Wessendorf <matzew@apache.org>
2014-01-20 14:37:40 +0100

    Adding unit tests for untested builder methods

--- src/test/java/org/jboss/aerogear/unifiedpush/message/UnifiedMessageTest.java
+++ src/test/java/org/jboss/aerogear/unifiedpush/message/UnifiedMessageTest.java
@@ -19,7 +19,11 @@ package org.jboss.aerogear.unifiedpush.message;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -84,6 +88,17 @@ public class UnifiedMessageTest {
     }
 
     @Test
+    public void simpleSelectiveMessageWithCategoriesAsSetTest() {
+        final Set<String> categories = new HashSet<String>();
+        categories.add("sports");
+        categories.add("world cup");
+        UnifiedMessage unifiedMessage = new UnifiedMessage.Builder()
+                .categories(categories)
+                .build();
+        assertEquals(2, unifiedMessage.getCategories().size());
+    }
+
+    @Test
     public void simplePushVersionMessageTest() {
         UnifiedMessage unifiedMessage = new UnifiedMessage.Builder()
                 .simplePush("version=1")
@@ -118,4 +133,29 @@ public class UnifiedMessageTest {
         assertNull(unifiedMessage.getAttributes().get("content-available"));
     }
 
+    @Test
+    public void customAttributes() {
+        UnifiedMessage unifiedMessage = new UnifiedMessage.Builder()
+                .alert("Hello from Java Sender API, via JUnit")
+                .sound("default")
+                .attribute("foo-key", "foo-value")
+                .attribute("bar-key", "bar-value")
+                .build();
+        assertEquals("foo-value", unifiedMessage.getAttributes().get("foo-key"));
+        assertEquals("bar-value", unifiedMessage.getAttributes().get("bar-key"));
+    }
+
+    @Test
+    public void customAttributesAsMap() {
+        final Map<String, Object> customAttributes = new HashMap<String, Object>();
+        customAttributes.put("foo-key", "foo-value");
+        customAttributes.put("bar-key", "bar-value");
+        UnifiedMessage unifiedMessage = new UnifiedMessage.Builder()
+                .alert("Hello from Java Sender API, via JUnit")
+                .sound("default")
+                .attributes(customAttributes)
+                .build();
+        assertEquals("foo-value", unifiedMessage.getAttributes().get("foo-key"));
+        assertEquals("bar-value", unifiedMessage.getAttributes().get("bar-key"));
+    }
 }
