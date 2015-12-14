26f47a3b8ece22a7669998ddcecd281c542fb344
sebastienblanc <scm.blanc@gmail.com>
2015-12-14 16:30:54 +0100

    simpler check

--- src/main/java/org/jboss/aerogear/unifiedpush/utils/PushConfiguration.java
+++ src/main/java/org/jboss/aerogear/unifiedpush/utils/PushConfiguration.java
@@ -65,7 +65,9 @@ public class PushConfiguration {
                     new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream(location)));
             Gson gson = new Gson();
             pushConfiguration =  gson.fromJson(bufferedReader, PushConfiguration.class);
-            pushConfiguration.setServerUrl(!pushConfiguration.getServerUrl().endsWith("/") ? pushConfiguration.getServerUrl() + '/' : pushConfiguration.getServerUrl());
+            if (!pushConfiguration.getServerUrl().endsWith("/")) {
+                pushConfiguration.setServerUrl(pushConfiguration.getServerUrl() + '/');
+            }
         }
        finally {
            if (bufferedReader != null) {
