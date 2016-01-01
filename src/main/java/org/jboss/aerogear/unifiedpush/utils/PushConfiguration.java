3931e1f120a6804db44a11e848ef4b55e5ce8b32
vevusio <vevusio@gmail.com>
2016-01-01 13:59:47 +0100

    cleaning up wrong import that slipped in (my ide isn't configured)

--- src/main/java/org/jboss/aerogear/unifiedpush/utils/PushConfiguration.java
+++ src/main/java/org/jboss/aerogear/unifiedpush/utils/PushConfiguration.java
@@ -21,7 +21,6 @@ import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Connection;
 import java.util.logging.Logger;
 
 public class PushConfiguration {
